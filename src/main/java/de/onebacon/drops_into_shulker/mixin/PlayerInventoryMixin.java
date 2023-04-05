a6c1ffced310e91c5ed8f297c70ce6b6731d3d0c
1bacon <marc.hadel@gmail.com>
2023-04-05 18:24:11 +0200

    Added Comments and simplified logic, bump 1.19.4

--- src/main/java/de/onebacon/drops_into_shulker/mixin/PlayerInventoryMixin.java
+++ src/main/java/de/onebacon/drops_into_shulker/mixin/PlayerInventoryMixin.java
@@ -1,14 +1,11 @@
 package de.onebacon.drops_into_shulker.mixin;
 
 
-import de.onebacon.drops_into_shulker.DropsIntoShulker;
 import de.onebacon.drops_into_shulker.ShulkerInventoryWrapper;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Nameable;
@@ -30,36 +27,35 @@ public abstract class PlayerInventoryMixin implements Inventory, Nameable {
     @Inject(method = "insertStack", at = @At("HEAD"), cancellable = true)
     private void insertStack(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         ItemStack offhand_item = this.offHand.get(0);
-        Block offhand_block = offhand_item.getItem() instanceof BlockItem ? ((BlockItem)offhand_item.getItem()).getBlock() : Blocks.AIR;
+        Block offhand_block = Block.getBlockFromItem(offhand_item.getItem());
 
+        if (!(offhand_block instanceof ShulkerBoxBlock)) {
+            return; // Offhand is not a Shulker
+        }
 
-        if (Block.getBlockFromItem(offhand_item.getItem()) instanceof ShulkerBoxBlock) {
+        NbtCompound tag = offhand_item.getOrCreateSubNbt("BlockEntityTag");
+        ShulkerInventoryWrapper temp_shulker = new ShulkerInventoryWrapper(BlockPos.ORIGIN, offhand_block.getDefaultState());
 
-            NbtCompound tag = offhand_item.getOrCreateSubNbt("BlockEntityTag");
-            ShulkerInventoryWrapper temp_shulker = new ShulkerInventoryWrapper(BlockPos.ORIGIN, offhand_block.getDefaultState());
+        if (!temp_shulker.canInsert(0, stack, null)) {
+            return; //Prevent shulker stacking
+        }
 
-            DropsIntoShulker.LOGGER.info(BlockPos.ORIGIN.toString());
-            DropsIntoShulker.LOGGER.info(BlockPos.ORIGIN.toShortString());
+        //Fill temp_shulker with off_hand contents
+        temp_shulker.readNbt(tag);
 
-            if (!temp_shulker.canInsert(0, stack, null))
-            {   //Prevent shulker stacking
-                return;
-            }
+        //Try to add the collected stack to temp_shulker
+        ItemStack return_stack = temp_shulker.addStack(stack);
 
-            temp_shulker.readNbt(tag);
+        //Save the inventory of temp_shulker
+        NbtCompound nbt_out = new NbtCompound();
+        temp_shulker._writeNbt(nbt_out);
 
-            ItemStack return_stack = temp_shulker.addStack(stack);
-
-
-            NbtCompound nbt_out = new NbtCompound();
-            temp_shulker._writeNbt(nbt_out);
-
-            if (return_stack.getCount() != stack.getCount()) {
-                stack.setCount(return_stack.getCount());
-                tag.put("Items", nbt_out.get("Items"));
-                if (return_stack.isEmpty()){
-                    cir.setReturnValue(true);
-                }
+        // Checking if they changed is not really necessary, but I don't want to change the NBT when nothing happened.
+        if (return_stack.getCount() != stack.getCount()) {
+            stack.setCount(return_stack.getCount());
+            tag.put("Items", nbt_out.get("Items"));
+            if (return_stack.isEmpty()) {
+                cir.setReturnValue(true);
             }
         }
     }
